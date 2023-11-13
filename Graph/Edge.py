a8f73ad5c596a676005e63ce7acc0c9277311af4
atul-ramkrishnan <atul.ramkrishnan97@gmail.com>
2023-11-13 17:53:38 -0500

    Add getters and setters to Edge

--- Graph/Edge.py
+++ Graph/Edge.py
@@ -3,5 +3,17 @@ class Edge:
         self.capacity = capacity
         self.flow = flow
 
+    def get_flow(self):
+        return self.flow
+    
+    def set_flow(self, flow):
+        self.flow = flow
+
+    def get_capacity(self):
+        return self.capacity
+    
+    def set_capacity(self, capacity):
+        self.capacity = capacity
+        
     def __repr__(self):
         return f"Edge(capacity={self.capacity}, flow={self.flow})"
