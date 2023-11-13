979283a5dc02af6b6f423f8383ad8c041357fd8a
atul-ramkrishnan <atul.ramkrishnan97@gmail.com>
2023-11-13 17:55:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ from augmentation_strategy.BFS import BFS
 from Graph.Graph import Graph
 from definitions import DATA_DIR
 import os
+from FordFulkerson import FordFulkerson
 
 
 def main():
     graph = Graph()
     graph.load_from_csv(os.path.join(DATA_DIR, "simple_graph.csv"))
-    print(graph)
-    # graph.save_as_csv(os.path.join(DATA_DIR, "simple_graph_saved.csv"))
-    # print(graph.get_adjacent_vertices("0"))
+    # print(graph)
     bfs = BFS()
-    res = bfs.execute(graph, "0", "5")
-    print(res)
+    ff = FordFulkerson(strategy=bfs)
+    print(ff.get_flow(graph, "S", "T"))
+
+
 
 if __name__ == "__main__":
     main()
