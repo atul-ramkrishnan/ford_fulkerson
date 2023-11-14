cd85a72b194911d47424dae4debe2bf683ccba8d
atul-ramkrishnan <atul.ramkrishnan97@gmail.com>
2023-11-14 18:27:23 -0500

    Code beautification

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
+import random
 from augmentation_strategy.BFS import BFS
 from graph.Graph import Graph
 from graph.GraphGenerator import GraphGenerator
 from definitions import DATA_DIR
-import os
 from FordFulkerson import FordFulkerson
+
 
 
 def main():
@@ -15,14 +17,16 @@ def main():
     # print(ff.get_flow(graph, "S", "T"))
     # test_dict = {}
     # print(test_dict is not None)
-    n = 100
+    n = 30
     r = 0.2
     upperCap = 2
     graphGenerator = GraphGenerator(n, r, upperCap)
     graph = graphGenerator.generate()
-    graph.save_as_csv(os.path.join(DATA_DIR, "test_new_graph_gen.csv"))
-
-
+    graph.save_as_csv(os.path.join(DATA_DIR, f"test_n_{n}_r_{r}_upperCap_{upperCap}.csv"))
+    bfs = BFS()
+    source = str(random.randint(0, n))
+    farthestVertex = bfs.get_farthest_vertex(graph, source) 
+    print(source, farthestVertex)
 
 if __name__ == "__main__":
     main()
