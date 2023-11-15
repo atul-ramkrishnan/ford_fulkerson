151351a2a9ec1b1a50dcb091be76111e5f43fb06
atul-ramkrishnan <atul.ramkrishnan97@gmail.com>
2023-11-15 17:59:35 -0500

    Change get_farthest_vertex to get_longest_acyclic_path

--- augmentation_strategy/BFS.py
+++ augmentation_strategy/BFS.py
@@ -15,14 +15,26 @@ class BFS(AugmentationStrategy):
                     visited.append(neighbour)
                     queue.append((neighbour, path + [neighbour]))
 
-    def get_farthest_vertex(self, graph, source):
+    def get_longest_acyclic_path(self, graph, source):
         visited = {}
         queue = [source]
+        predecessors = {source: None}
         visited[source] = 0
+        
         while queue:
             m = queue.pop()
             for neighbour in graph.get_adjacent_vertices(m):
                 if neighbour not in visited:
                     visited[neighbour] = visited[m] + 1
+                    predecessors[neighbour] = m
                     queue.append(neighbour)
-        return max(visited, key=visited.get)
+
+        farthest_vertex = max(visited, key=visited.get)
+        longest_acyclic_path = []
+        current_vertex = farthest_vertex
+
+        while current_vertex is not None:
+            longest_acyclic_path.insert(0, current_vertex)
+            current_vertex = predecessors[current_vertex]
+
+        return longest_acyclic_path
